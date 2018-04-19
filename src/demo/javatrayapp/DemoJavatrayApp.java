4420191be6cd2c75642d3e9a81d14ba1680e0282
o.levitskiy <o.levitskiy@GR-MS-IT03.ipmatika.local>
2018-04-19 17:32:38 +0300

    Test - 1.3

--- src/demo/javatrayapp/DemoJavatrayApp.java
+++ src/demo/javatrayapp/DemoJavatrayApp.java
@@ -4,20 +4,46 @@
 package demo.javatrayapp;
 
 import java.awt.AWTException;
+import java.awt.CheckboxMenuItem;
 import java.awt.Image;
+import java.awt.Menu;
+import java.awt.MenuItem;
+import java.awt.PopupMenu;
 import java.awt.SystemTray;
 import java.awt.Toolkit;
 import java.awt.TrayIcon;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.File;
-import javax.imageio.ImageIO;
-import javax.swing.ImageIcon;
-import sun.tools.jar.Main;
 
 public class DemoJavatrayApp {  
 
   public static void main(String[] a) throws Exception {
+      
+    PopupMenu popup = new PopupMenu(); 
+     
+    MenuItem aboutItem = new MenuItem("About");
+    CheckboxMenuItem cb1 = new CheckboxMenuItem("Set auto size");
+    CheckboxMenuItem cb2 = new CheckboxMenuItem("Set tooltip");
+    Menu displayMenu = new Menu("Display");
+    MenuItem errorItem = new MenuItem("Error");
+    MenuItem warningItem = new MenuItem("Warning");
+    MenuItem infoItem = new MenuItem("Info");
+    MenuItem noneItem = new MenuItem("None");
+    MenuItem exitItem = new MenuItem("Exit");
+
+    //Add components to pop-up menu
+    popup.add(aboutItem);
+    popup.addSeparator();
+    popup.add(cb1);
+    popup.add(cb2);
+    popup.addSeparator();
+    popup.add(displayMenu);
+    displayMenu.add(errorItem);
+    displayMenu.add(warningItem);
+    displayMenu.add(infoItem);
+    displayMenu.add(noneItem);
+    popup.add(exitItem);      
+      
     String workingDir = System.getProperty("user.dir"); 
     System.out.println("Current working directory : " + workingDir);      
     Image image = Toolkit.getDefaultToolkit().getImage(workingDir+"\\tray2.gif");
@@ -27,11 +53,19 @@ public class DemoJavatrayApp {
       SystemTray tray = SystemTray.getSystemTray();
 
       trayIcon.setImageAutoSize(true);
+      trayIcon.setPopupMenu(popup);
       trayIcon.addActionListener(new ActionListener() {
         public void actionPerformed(ActionEvent e) {
-          System.out.println("In here");
-          trayIcon.displayMessage("Tester!", "TEST Message", TrayIcon.MessageType.INFO);
+              System.out.println("In here");
+              trayIcon.displayMessage("Tester!", "TEST Message", TrayIcon.MessageType.INFO);
         }
+      });
+      
+      aboutItem.addActionListener(new ActionListener() {        
+        public void actionPerformed(ActionEvent e) {
+            System.out.println("In here");
+            trayIcon.displayMessage("Tester!", "About APP", TrayIcon.MessageType.INFO);
+          }
       });
 
       try {
